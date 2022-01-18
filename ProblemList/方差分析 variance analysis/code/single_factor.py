6b0e7dfb8c21cd708ddc4f86f080c0d4a9b94c67
inwinleo <inwin_leo@qq.com>
2022-01-18 22:41:50 +0800

    single factor done

--- ProblemList/方差分析 variance analysis/code/single_factor.py
+++ ProblemList/方差分析 variance analysis/code/single_factor.py
@@ -1,7 +1,15 @@
 import numpy as np
+from scipy.stats import f as F
 
 np.random.seed(123)
-raw_data = np.random.randint(-10, 50, size=(10, 8))
+raw_data = np.random.randint(-10, 50, size=(3, 8))
+'''
+随机生成三组数据，每组8个样本
+取消下面注释的一行来模拟存在组间差异
+'''
+#raw_data[0] = np.linspace(1, 10, 8)
+
+
 print("raw data is:\n", raw_data, '\n')
 
 column_n = raw_data.shape[1]
@@ -19,3 +27,12 @@ print('Variance between groups is:', Variance_between_groups, '\n')
 F_examine = Variance_between_groups / \
     intraclass_variance*((column_n-1)*row_n)/(row_n-1)
 print("Factor F is:", F_examine, '\n')
+
+
+alpha = 0.05  # 调整显著性水平，值越小判定越严格
+P = F.pdf(F_examine, (column_n-1)*row_n, (row_n-1))
+if (P <= alpha):
+    print("F_examine passed!", end='')
+else:
+    print("F_examine not passed!", end='')
+print(" P is:", P)
